28e22f64e4a5a12242f5235c7f29f0a7bb286382
Kirill Nikolaev <kirill.nikolaev.an@gmail.com>
2022-10-08 03:52:17 +0300

    create tables and models

---
+++ src/main/java/ru/library/models/Book.java
@@ -0,0 +1,52 @@
+package ru.library.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Book {
+    int id;
+    int name;
+    int author;
+    int year;
+    int personId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public int getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(int author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+}
---
+++ src/main/java/ru/library/models/Person.java
@@ -0,0 +1,39 @@
+package ru.library.models;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+    private int id;
+    private String fullName;
+    private int birthYear;
+
+    public Person() {
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+}
